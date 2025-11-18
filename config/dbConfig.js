bba5e1c0706107b2d9f4a52c1ea577e354df00c5
L-Abimanyu <abim7562@gmail.com>
2025-11-18 07:57:31 +0530

    Adding yaml file

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 
 const MONGO_URI = process.env.MONGO_URI;
 
-console.log(MONGO_URI, 'MONGO_URI');
-
 const dbConfig = async () => {
   try {
     await mongoose.connect(MONGO_URI);
